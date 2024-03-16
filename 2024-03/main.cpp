cf45bbb466ad5cf73d5243d7dc5bbb84bccc5fdd
Johnny Thompson <jthompson@tmg.dev>
2024-03-15 19:57:16 -0700

    Added cache hits from episode 8

---
+++ 2024-03/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	int array[3];
+	
+	array[0] = 69;
+	1[array] = 42;
+	(1 + 1)[array] = 420;
+	
+	std::cout << 0[array]; // 69
+	std::cout << array[1]; // 42
+	std::cout << 2[array]; // 420
+}
